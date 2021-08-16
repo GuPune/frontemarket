4864ebab219bee45ce37a6e3e6eaa652161dd2e6
supawadee2541 <supawadee.ss92@gmail.com>
2021-08-16 13:32:23 +0700

    update edit
    edit category

--- components/Ads.vue
+++ components/Ads.vue
@@ -6,56 +6,56 @@
       
      
             <div class="carousel">
-    <ul class="slides">
-      <input type="radio" name="radio-buttons" id="img-1" checked />
-      <li class="slide-container">
-        <div class="slide-image">
-          <img src="http://i1.wp.com/www.thaismescenter.com/wp-content/uploads/2019/09/941.jpg?resize=800%2C445">
-        </div>
-        <div class="carousel-controls">
-          <label for="img-3" class="prev-slide">
-            <span>&lsaquo;</span>
-          </label>
-          <label for="img-2" class="next-slide">
-            <span>&rsaquo;</span>
-          </label>
-        </div>
-      </li>
-      <input type="radio" name="radio-buttons" id="img-2" />
-      <li class="slide-container">
-        <div class="slide-image">
-          <img src="https://image.makewebeasy.net/makeweb/0/r9drrIwcs/DefaultData/image1_3.jpeg">
-        </div>
-        <div class="carousel-controls">
-          <label for="img-1" class="prev-slide">
-            <span>&lsaquo;</span>
-          </label>
-          <label for="img-3" class="next-slide">
-            <span>&rsaquo;</span>
-          </label>
-        </div>
-      </li>
-      <input type="radio" name="radio-buttons" id="img-3" />
-      <li class="slide-container">
-        <div class="slide-image">
-          <img src="https://www.passiongen.com/wp-content/uploads/2018/11/03-Promotion-FB-1.png">
-        </div>
-        <div class="carousel-controls">
-          <label for="img-2" class="prev-slide">
-            <span>&lsaquo;</span>
-          </label>
-          <label for="img-1" class="next-slide">
-            <span>&rsaquo;</span>
-          </label>
-        </div>
-      </li>
-      <div class="carousel-dots">
-        <label for="img-1" class="carousel-dot" id="img-dot-1"></label>
-        <label for="img-2" class="carousel-dot" id="img-dot-2"></label>
-        <label for="img-3" class="carousel-dot" id="img-dot-3"></label>
-      </div>
-    </ul>
-  </div>
+              <ul class="slides">
+                <input type="radio" name="radio-buttons" id="img-1" checked />
+                <li class="slide-container">
+                  <div class="slide-image">
+                    <img  src="../assets/banner1.png">
+                  </div>
+                  <div class="carousel-controls">
+                    <label for="img-3" class="prev-slide">
+                      <span>&lsaquo;</span>
+                    </label>
+                    <label for="img-2" class="next-slide">
+                      <span>&rsaquo;</span>
+                    </label>
+                  </div>
+                </li>
+                <input type="radio" name="radio-buttons" id="img-2" />
+                <li class="slide-container">
+                  <div class="slide-image">
+                    <img src="../assets/banner1.png">
+                  </div>
+                  <div class="carousel-controls">
+                    <label for="img-1" class="prev-slide">
+                      <span>&lsaquo;</span>
+                    </label>
+                    <label for="img-3" class="next-slide">
+                      <span>&rsaquo;</span>
+                    </label>
+                  </div>
+                </li>
+                <input type="radio" name="radio-buttons" id="img-3" />
+                <li class="slide-container">
+                  <div class="slide-image">
+                    <img src="../assets/banner1.png">
+                  </div>
+                  <div class="carousel-controls">
+                    <label for="img-2" class="prev-slide">
+                      <span>&lsaquo;</span>
+                    </label>
+                    <label for="img-1" class="next-slide">
+                      <span>&rsaquo;</span>
+                    </label>
+                  </div>
+                </li>
+                <div class="carousel-dots">
+                  <label for="img-1" class="carousel-dot" id="img-dot-1"></label>
+                  <label for="img-2" class="carousel-dot" id="img-dot-2"></label>
+                  <label for="img-3" class="carousel-dot" id="img-dot-3"></label>
+                </div>
+              </ul>
+            </div>
 
 
 
@@ -76,11 +76,11 @@
     <div class="row adsmobile">
             <div id="slider">
           <figure>
-                <div class="image"><img class="imgads" src="http://i1.wp.com/www.thaismescenter.com/wp-content/uploads/2019/09/941.jpg?resize=800%2C445" alt="Slider 1"></div>
-                <div class="image"><img class="imgads" src="https://image.makewebeasy.net/makeweb/0/r9drrIwcs/DefaultData/image1_3.jpeg" alt="Slider 2"></div>
-                <div class="image"><img class="imgads" src="https://www.passiongen.com/wp-content/uploads/2018/11/03-Promotion-FB-1.png" alt="Slider 3"></div>
-                <div class="image"><img class="imgads" src="https://www.potatotechs.com/wp-content/uploads/2018/04/asdasd-07.jpg" alt="Slider 4"></div>
-                <div class="image"><img class="imgads" src="https://static.wixstatic.com/media/a27d24_57acb30ba1de405c82be4a75a8c1fd75~mv2.png/v1/fill/w_1000,h_667,al_c,usm_0.66_1.00_0.01/a27d24_57acb30ba1de405c82be4a75a8c1fd75~mv2.png" alt="Slider 1"></div>
+                <div class="image"><img class="imgads" src="../assets/banner1.png" alt="Slider 1"></div>
+                <div class="image"><img class="imgads" src="../assets/banner1.png" alt="Slider 2"></div>
+                <div class="image"><img class="imgads" src="../assets/banner1.png" alt="Slider 3"></div>
+                <div class="image"><img class="imgads" src="../assets/banner1.png" alt="Slider 4"></div>
+                <div class="image"><img class="imgads" src="../assets/banner1.png" alt="Slider 1"></div>
             </figure>         
             </div>
 
--- components/Categories.vue
+++ components/Categories.vue
@@ -1,66 +1,54 @@
 <template>
   <section id="categories">
     
-    <div class="row categoryweb">
-      <div class="col-8">
-              <hr class="hrsection">
-              <h3>ประเภทสินค้า</h3>
+    <center><div class="row categoryweb col-9">
+    
+        
+            <div class="col-sm">
+              <img class="cateimg" src="../assets/cate1.png">
 
-             <div class="slider">
-                        <img class="img-slider" src="https://www.tcijthai.com/ckfinder/userfiles/images/5(230).jpg">
-                        <img class="img-slider" src="https://petmaya.com/wp-content/uploads/2017/04/fruit-facts-01.jpg">
-                        <img class="img-slider" src="https://images.squarespace-cdn.com/content/v1/558c7947e4b0b3c68249cc4b/1534939901621-4QLBIAEZZ6OJANTDIUFM/31361d5c-4c5a-47bf-b095-c42e9bc463a6-1.jpg">
-                        <img class="img-slider" src="https://i1.wp.com/data.thaiseedshop.com/uploads/thaiseedshop-sej-02.png">
-                        <img class="img-slider" src="https://www.naijokaset.com/wp-content/uploads/2019/11/60306507.jpg">
-                        <img class="img-slider" src="https://www.tcijthai.com/ckfinder/userfiles/images/5(230).jpg">
-                        <img class="img-slider" src="https://petmaya.com/wp-content/uploads/2017/04/fruit-facts-01.jpg">
-                        <img class="img-slider" src="https://images.squarespace-cdn.com/content/v1/558c7947e4b0b3c68249cc4b/1534939901621-4QLBIAEZZ6OJANTDIUFM/31361d5c-4c5a-47bf-b095-c42e9bc463a6-1.jpg">
-                        <img class="img-slider" src="https://i1.wp.com/data.thaiseedshop.com/uploads/thaiseedshop-sej-02.png">
-                        <img class="img-slider" src="https://www.naijokaset.com/wp-content/uploads/2019/11/60306507.jpg">
-                    </div>
-                
+            </div>
 
-              
+            <div class="col-sm">
+              <img class="cateimg" src="../assets/cate2.png">
 
-                   
+            </div>
 
-                    
+            <div class="col-sm">
+              <img class="cateimg" src="../assets/cate3.png">
 
+            </div>
 
-              <hr class="hrsection">
+      
 
-      </div>
-
-    </div>
+      
+  </div></center>
 
 
 <!-- <------------Mobile---------------------------->
 
-    <div class="row categorymobile">
-      <div class="col-12">
-              <hr class="hrsection">
-              <h5>ประเภทสินค้า</h5>
-                 <div class="slider">
-                        <img class="img-slider" src="https://www.tcijthai.com/ckfinder/userfiles/images/5(230).jpg">
-                        <img class="img-slider" src="https://petmaya.com/wp-content/uploads/2017/04/fruit-facts-01.jpg">
-                        <img class="img-slider" src="https://images.squarespace-cdn.com/content/v1/558c7947e4b0b3c68249cc4b/1534939901621-4QLBIAEZZ6OJANTDIUFM/31361d5c-4c5a-47bf-b095-c42e9bc463a6-1.jpg">
-                        <img class="img-slider" src="https://i1.wp.com/data.thaiseedshop.com/uploads/thaiseedshop-sej-02.png">
-                        <img class="img-slider" src="https://www.naijokaset.com/wp-content/uploads/2019/11/60306507.jpg">
-                        <img class="img-slider" src="https://www.tcijthai.com/ckfinder/userfiles/images/5(230).jpg">
-                        <img class="img-slider" src="https://petmaya.com/wp-content/uploads/2017/04/fruit-facts-01.jpg">
-                        <img class="img-slider" src="https://images.squarespace-cdn.com/content/v1/558c7947e4b0b3c68249cc4b/1534939901621-4QLBIAEZZ6OJANTDIUFM/31361d5c-4c5a-47bf-b095-c42e9bc463a6-1.jpg">
-                        <img class="img-slider" src="https://i1.wp.com/data.thaiseedshop.com/uploads/thaiseedshop-sej-02.png">
-                        <img class="img-slider" src="https://www.naijokaset.com/wp-content/uploads/2019/11/60306507.jpg">
-                  </div>
-             
+   <center> <div class="row categorymobile">
+
+            <div class="col">
+              <img class="cateimg" src="../assets/cate1.png">
+
+            </div>
+
+            <div class="col">
+              <img class="cateimg" src="../assets/cate2.png">
+
+            </div>
+
+            <div class="col">
+              <img class="cateimg" src="../assets/cate3.png">
+
+            </div>
+
+    
 
 
 
-              <hr class="hrsection">
-
-      </div>
-
-    </div>
+    </div></center>
 
 
 
