469ae822a2699b2c26d9fe72397990f5f137d056
supawadee2541 <supawadee.ss92@gmail.com>
2021-08-19 20:31:04 +0700

    update
    profile

--- pages/profile/userprofile.vue
+++ pages/profile/userprofile.vue
@@ -2,12 +2,106 @@
     <section id="Userprofile">
         <Nav />
 
-        <div class="row profieweb">
+        <div class="container userprofileweb col-9">
+
+          
+                <div class="bg-white">
+                        <div class="container py-5">
+                            <div class="media col-md-10 col-lg-8 col-xl-7 p-0 my-4 mx-auto">
+                            <img src="https://www.arianagrande.com/sites/g/files/aaj12746/f/styles/suzuki_breakpoints_image_tablet/public/positions-final_0.jpg" alt class="d-block ui-w-100 rounded-circle profile">
+                            <div class="media-body ml-5">
+                                <h4 class="font-weight-bold mb-4">Ariana Grande</h4>
+                                <div class="text-muted mb-4">
+                                ถนน ชัยพฤกษ์ ตำบลในเมือง อำเภอเมืองขอนแก่น ขอนแก่น 40000
+                                </div>
+
+                                <div class="text-muted mb-4">
+                                <i class="fa fa-phone" aria-hidden="true"> :</i> 085-4476635<br>
+                                <i class="fa fa-envelope" aria-hidden="true"> :</i> ariana@mail.com
+                                </div>
+
+                                
+                                <div class="text-muted mb-4">
+                                <small class="btn btn-primary">อัปโหลดรูปภาพ</small>
+                                </div>
+                                <a class="d-inline-block text-dark">
+                                <strong>ประเภท</strong>
+                                <small class="text-muted">โปรไฟล์ผู้ซื้อ</small>
+                                </a>
+                            </div>
+                            </div>
+                        </div>
+
+                </div>
+                   
+        </div>
+
+
+
+       
+
+
+
+
+        <!------------------------------------------------------------------------ MOBILE ---------------------------------------------------------------------->
+         <div class="userprofilemobile">
+
+                <div class="bg-white">
+                        <div class="container py-5">
+                            <div class="media col-md-10 col-lg-8 col-xl-7 p-0 my-4 mx-auto">
+                            <img src="https://www.arianagrande.com/sites/g/files/aaj12746/f/styles/suzuki_breakpoints_image_tablet/public/positions-final_0.jpg" alt class="d-block ui-w-100 rounded-circle profile">
+                             <div class="media-body ml-5">
+                                <h4 class="font-weight-bold mb-4">Ariana Grande</h4>
+                                <div class="text-muted mb-4">
+                                ถนน ชัยพฤกษ์ ตำบลในเมือง อำเภอเมืองขอนแก่น ขอนแก่น 40000
+                                </div>
+
+                                <div class="text-muted mb-4">
+                                <i class="fa fa-phone" aria-hidden="true"> :</i> 085-4476635<br>
+                                <i class="fa fa-envelope" aria-hidden="true"> :</i> ariana@mail.com
+                                </div>
+
+                                
+                                <div class="text-muted mb-4">
+                                <small class="btn btn-primary">อัปโหลดรูปภาพ</small>
+                                </div>
+                                <a class="d-inline-block text-dark">
+                                <strong>ประเภท</strong>
+                                <small class="text-muted">โปรไฟล์ผู้ซื้อ</small>
+                                </a>
+                            </div>
+                            </div>
+                        </div>
+
+                </div>
+
+      
+
+        </div>
+
 
     
 
+                     
 
-        </div>
+
+      
+       
+
+   
+                
+
+
+       
+
+
+
+
+
+
+
+                       
+
 
         <Footer />
 
