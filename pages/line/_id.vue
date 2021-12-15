435a9712a98089550147e2666edb186dfed6a396
Rkknoob <iboonkhet@gmail.com>
2021-12-15 22:14:25 +0700

    update
    update

--- pages/line/_id.vue
+++ pages/line/_id.vue
@@ -86,29 +86,175 @@
                   <div class="tab-content">
                         <div class="tab-pane active" id="category0">
   
-  <div class="bg-white p-3 mb-1" data-toggle="modal" data-target="#exampleModal12389">
+  <div class="tab-pane active" id="category7">
+          <!-- อาหาร -->
+                    <div class="bg-white p-3 mb-1" data-toggle="modal" data-target="#exampleModal14870">
             <div class="row" style="">
               <div class="col-3">
                                   <!-- <img src="https://image.makewebeasy.net/noimage.png" width="100%"/> -->
-                  <img src="https://gg.dinovery.app/myadmin/images/menu/menu_20200505154848.jpg" width="100%">
+                  <img src="https://gg.dinovery.app/myadmin/images/menu/menu_20200510122816.jpg" width="100%">
 
                               </div>
               <div class="col-6">
                 <div>
-                  Cable Type-C 1M (RC-103a,Silver) - สายชาร์จ REMAX  สินค้าประกัน1ปีพร้อมกล่อง<br>
-                  <span style="font-size:0.6em;">รายละเอียดเกี่ยวกับสินค้า 
-สายชาร์จ Remax สายชาร์จแบบ usb to Type-C ออกแบบดีไซน์สวยงามจากผู้ผลิตอุปกรณ์ Smart Phone ชั้นนำของโลก Remax สินค้าจากฮ่องกง มาตรฐานส่งออกทั่วโลกสินค้ารับประกันพร้อมกล่องโดย รีแมค(ไทยแลนด์) Remax Thailand สายชาร์จ คุณภาพดีเพื่อเพิ่มความคงทน ทนทาน สายทองแดงภายในคุณภาพสูง เพื่อเพิ่มประสิทธิภาพในการชาร์จและ ส่งข้อมูล 
-คุณสมบัติพิเศษ
- -สายชาร์จ usb to Type-C
-- Smart Chip รองรับการปล่อยกระแสไฟได้ดีขึ้น 
--สายชาร์จแบบสายซิลิโคนหุ้มด้วยผ้าแบบถักอีกชั้น เพิ่มความแข็งแรงให้กับสาย 
--หัวชาร์จ Type-C แบบแนวนอน 
--รองรับกระแสไฟสำหรับชาร์จโทรศัพท์ สูงสุด 2.4A(MAX) 
--มีไฟแสดงสถานะการทำงาน 
--สายยาว 100 cm. 
--สายทองแดงภายใน ใหญ่และ หนากว่าสายปกติ เพื่อช่วยเพิ่มความเร็วในการชาร์จ และรับส่งข้อมูล 
-วิธีใช้งาน
- เสียบชาร์จหรือใช้สำหรับเชื่อมต่อรับ/ส่งข้อมูลกับอุปกรณ์ที่รองรับ Type-C</span><br>
+                  Ultra USB 3.0 Flash Drive 32GB - Sandisk  ประกันพร้อมกล่อง5ปี<br>
+                  <span style="font-size:0.6em;"></span><br>
+                  <!-- <span style="font-size:0.8em;"></span> -->
+                </div>
+              </div>
+              <div class="col-3 text-right">
+                <div> 299.00</div>
+              </div>
+            </div>
+          </div>
+          <!-- Modal -->
+          <form action="https://dinovery.app/app/index.php/Order/order_add" method="post" accept-charset="utf-8">
+          <div class="modal" id="exampleModal14870" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header" style="font-size:1.3em;">
+                  <div class="modal-title">Ultra USB 3.0 Flash Drive 32GB - Sandisk  ประกันพร้อมกล่อง5ปี</div>
+                  <div class="modal-title"> 299.00</div>
+                </div>
+                <div class="modal-body">
+                  <div class="">
+                                                              <!-- <center><img src="https://gg.dinovery.app/myadmin/images/menu/menu_20200510122816.jpg" width="80%"/></center> -->
+                      <br>
+                                        <div class="form-group">
+                                            <label>ตัวเลือก </label>
+                                          <hr>
+                    
+                      <label>หมายเหตุ</label>
+                      <small>(ไม่ระบุก็ได้)</small>
+                      <textarea name="partner_menu_detail" autofocus="autofocus" class="form-control form-control-sm input-border-bottom" placeholder="รายละเอียด และข้อมูลเพิ่มเติมของรายการนี้!"></textarea>
+                    </div>
+                  </div>
+                  <script>
+				   function incrementValue14870()
+					{
+						var value = parseInt(document.getElementById('number14870').value, 10);
+						value = isNaN(value) ? 0 : value;
+						value++;
+						document.getElementById('number14870').value = value;
+					}
+				  </script>
+                  <script>
+				   function decreaseValue14870()
+					{
+						var value = parseInt(document.getElementById('number14870').value, 10);
+						value = isNaN(value) ? 0 : value;
+						value--;
+						if(value<1){
+							document.getElementById('number14870').value = 1;
+						}else{
+							document.getElementById('number14870').value = value;
+						}
+					}
+				  </script>
+                  <div class="text-center mb-3" style="font-size:2em;"> <i class="far fa-minus-square text-muted mr-2" onclick="decreaseValue14870()"></i>
+                    <input type="text" name="partner_menu_amount" id="number14870" min="1" value="1" style=" width:15%; text-align:center; ">
+                    <i class="far fa-plus-square text-muted ml-2" onclick="incrementValue14870()"></i> </div>
+                  <div class="text-center">
+                    <input name="partner_id" type="hidden" value="2932">
+                    <input name="partner_menu_cost" type="hidden" value="299">
+                    <input name="partner_menu_id" type="hidden" value="14870">
+                    <input name="partner_menu_name" type="hidden" value="Ultra USB 3.0 Flash Drive 32GB - Sandisk  ประกันพร้อมกล่อง5ปี">
+                    <input class="btn btn-warning btn-block btn-lg" name="button" type="submit" value="เพิ่มไปยังตะกร้า">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </form>                    <div class="bg-white p-3 mb-1" data-toggle="modal" data-target="#exampleModal14871">
+            <div class="row" style="">
+              <div class="col-3">
+                                  <!-- <img src="https://image.makewebeasy.net/noimage.png" width="100%"/> -->
+                  <img src="https://gg.dinovery.app/myadmin/images/menu/menu_20200510122958.jpg" width="100%">
+
+                              </div>
+              <div class="col-6">
+                <div>
+                  MICRO SD Card 16GB/CL10 (REMAX)  ประกันพร้อมกล่อง5ปี<br>
+                  <span style="font-size:0.6em;"></span><br>
+                  <!-- <span style="font-size:0.8em;"></span> -->
+                </div>
+              </div>
+              <div class="col-3 text-right">
+                <div> 259.00</div>
+              </div>
+            </div>
+          </div>
+          <!-- Modal -->
+          <form action="https://dinovery.app/app/index.php/Order/order_add" method="post" accept-charset="utf-8">
+          <div class="modal" id="exampleModal14871" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header" style="font-size:1.3em;">
+                  <div class="modal-title">MICRO SD Card 16GB/CL10 (REMAX)  ประกันพร้อมกล่อง5ปี</div>
+                  <div class="modal-title"> 259.00</div>
+                </div>
+                <div class="modal-body">
+                  <div class="">
+                                                              <!-- <center><img src="https://gg.dinovery.app/myadmin/images/menu/menu_20200510122958.jpg" width="80%"/></center> -->
+                      <br>
+                                        <div class="form-group">
+                                            <label>ตัวเลือก </label>
+                                          <hr>
+                    
+                      <label>หมายเหตุ</label>
+                      <small>(ไม่ระบุก็ได้)</small>
+                      <textarea name="partner_menu_detail" autofocus="autofocus" class="form-control form-control-sm input-border-bottom" placeholder="รายละเอียด และข้อมูลเพิ่มเติมของรายการนี้!"></textarea>
+                    </div>
+                  </div>
+                  <script>
+				   function incrementValue14871()
+					{
+						var value = parseInt(document.getElementById('number14871').value, 10);
+						value = isNaN(value) ? 0 : value;
+						value++;
+						document.getElementById('number14871').value = value;
+					}
+				  </script>
+                  <script>
+				   function decreaseValue14871()
+					{
+						var value = parseInt(document.getElementById('number14871').value, 10);
+						value = isNaN(value) ? 0 : value;
+						value--;
+						if(value<1){
+							document.getElementById('number14871').value = 1;
+						}else{
+							document.getElementById('number14871').value = value;
+						}
+					}
+				  </script>
+                  <div class="text-center mb-3" style="font-size:2em;"> <i class="far fa-minus-square text-muted mr-2" onclick="decreaseValue14871()"></i>
+                    <input type="text" name="partner_menu_amount" id="number14871" min="1" value="1" style=" width:15%; text-align:center; ">
+                    <i class="far fa-plus-square text-muted ml-2" onclick="incrementValue14871()"></i> </div>
+                  <div class="text-center">
+                    <input name="partner_id" type="hidden" value="2932">
+                    <input name="partner_menu_cost" type="hidden" value="259">
+                    <input name="partner_menu_id" type="hidden" value="14871">
+                    <input name="partner_menu_name" type="hidden" value="MICRO SD Card 16GB/CL10 (REMAX)  ประกันพร้อมกล่อง5ปี">
+                    <input class="btn btn-warning btn-block btn-lg" name="button" type="submit" value="เพิ่มไปยังตะกร้า">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </form>                    <div class="bg-white p-3 mb-1" data-toggle="modal" data-target="#exampleModal14873">
+            <div class="row" style="">
+              <div class="col-3">
+                                  <!-- <img src="https://image.makewebeasy.net/noimage.png" width="100%"/> -->
+                  <img src="https://gg.dinovery.app/myadmin/images/menu/menu_20200510123427.jpg" width="100%">
+
+                              </div>
+              <div class="col-6">
+                <div>
+                  Micro SD Card 16GB-เมมโมรี่การ์ด Kingston สินค้าประกันพร้อมกล่อง5ปี<br>
+                  <span style="font-size:0.6em;">คุณสมบัติพิเศษ
+
+เมมโมรี่การ์ดสำหรับบันทึกข้อมูลให้กับอุปกรณ์ต่างๆ เช่น โทรศัพท์มือถือ เป็นต้นใช้งานได้กับอุปกรณ์หลากหลายเช่น โทรศัพท์,กล้องติดรถยนต์,เครื่องเล่น MP3,เครื่องเล่นเกมส์ เป็นต้นเมมโมรี่การ์ดมาตรฐาน Class 10</span><br>
                   <!-- <span style="font-size:0.8em;"></span> -->
                 </div>
               </div>
@@ -117,6 +263,68 @@
               </div>
             </div>
           </div>
+          <!-- Modal -->
+          <form action="https://dinovery.app/app/index.php/Order/order_add" method="post" accept-charset="utf-8">
+          <div class="modal" id="exampleModal14873" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header" style="font-size:1.3em;">
+                  <div class="modal-title">Micro SD Card 16GB-เมมโมรี่การ์ด Kingston สินค้าประกันพร้อมกล่อง5ปี</div>
+                  <div class="modal-title"> 199.00</div>
+                </div>
+                <div class="modal-body">
+                  <div class="">
+                    คุณสมบัติพิเศษ
+
+เมมโมรี่การ์ดสำหรับบันทึกข้อมูลให้กับอุปกรณ์ต่างๆ เช่น โทรศัพท์มือถือ เป็นต้นใช้งานได้กับอุปกรณ์หลากหลายเช่น โทรศัพท์,กล้องติดรถยนต์,เครื่องเล่น MP3,เครื่องเล่นเกมส์ เป็นต้นเมมโมรี่การ์ดมาตรฐาน Class 10                                          <!-- <center><img src="https://gg.dinovery.app/myadmin/images/menu/menu_20200510123427.jpg" width="80%"/></center> -->
+                      <br>
+                                        <div class="form-group">
+                                            <label>ตัวเลือก </label>
+                                          <hr>
+                    
+                      <label>หมายเหตุ</label>
+                      <small>(ไม่ระบุก็ได้)</small>
+                      <textarea name="partner_menu_detail" autofocus="autofocus" class="form-control form-control-sm input-border-bottom" placeholder="รายละเอียด และข้อมูลเพิ่มเติมของรายการนี้!"></textarea>
+                    </div>
+                  </div>
+                  <script>
+				   function incrementValue14873()
+					{
+						var value = parseInt(document.getElementById('number14873').value, 10);
+						value = isNaN(value) ? 0 : value;
+						value++;
+						document.getElementById('number14873').value = value;
+					}
+				  </script>
+                  <script>
+				   function decreaseValue14873()
+					{
+						var value = parseInt(document.getElementById('number14873').value, 10);
+						value = isNaN(value) ? 0 : value;
+						value--;
+						if(value<1){
+							document.getElementById('number14873').value = 1;
+						}else{
+							document.getElementById('number14873').value = value;
+						}
+					}
+				  </script>
+                  <div class="text-center mb-3" style="font-size:2em;"> <i class="far fa-minus-square text-muted mr-2" onclick="decreaseValue14873()"></i>
+                    <input type="text" name="partner_menu_amount" id="number14873" min="1" value="1" style=" width:15%; text-align:center; ">
+                    <i class="far fa-plus-square text-muted ml-2" onclick="incrementValue14873()"></i> </div>
+                  <div class="text-center">
+                    <input name="partner_id" type="hidden" value="2932">
+                    <input name="partner_menu_cost" type="hidden" value="199">
+                    <input name="partner_menu_id" type="hidden" value="14873">
+                    <input name="partner_menu_name" type="hidden" value="Micro SD Card 16GB-เมมโมรี่การ์ด Kingston สินค้าประกันพร้อมกล่อง5ปี">
+                    <input class="btn btn-warning btn-block btn-lg" name="button" type="submit" value="เพิ่มไปยังตะกร้า">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </form>                    <!-- Modal -->
+        </div>
         
 		</div>
         </div>
