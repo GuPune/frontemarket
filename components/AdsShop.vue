0fc997e3609e3d92966d264fb3ea9cc51af6b27f
Rkknoob <iboonkhet@gmail.com>
2022-01-03 23:07:26 +0700

    upa
    upa

--- components/AdsShop.vue
+++ components/AdsShop.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="asd-shop" style="padding-top:32px">
+<div class="asd-shop">
 <div v-if="ads_shop">
   <img loading="lazy" :src="Checkimage(ads_shop.images)" class="col-12 no-padding banner-icon" style="display: block;padding: 0;">
 </div>
