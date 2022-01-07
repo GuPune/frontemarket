01c7a3e1ff8abb13029708fdf432e4b29103076c
boonkhet idklever <iboonkhet@gmail.com>
2022-01-07 11:32:32 +0700

    Update Test.vue

--- components/Test.vue
+++ components/Test.vue
@@ -129,6 +129,24 @@ import { FETCH_CATEGORY_SHELL,FETCH_PRODUCT_FIND } from "../store/actions.type.j
   "speed": 500,
   "autoplaySpeed": 500,
   "responsive": [
+            {
+      "breakpoint": 1600,
+      "settings": {
+        "slidesToShow": 6,
+        "slidesToScroll": 4,
+        "infinite": true,
+        "dots": true
+      }
+    },
+          {
+      "breakpoint": 1500,
+      "settings": {
+        "slidesToShow": 5,
+        "slidesToScroll": 4,
+        "infinite": true,
+        "dots": true
+      }
+    },
       {
       "breakpoint": 1300,
       "settings": {
@@ -141,7 +159,7 @@ import { FETCH_CATEGORY_SHELL,FETCH_PRODUCT_FIND } from "../store/actions.type.j
     {
       "breakpoint": 1024,
       "settings": {
-        "slidesToShow": 6,
+        "slidesToShow": 4,
         "slidesToScroll": 6,
         "infinite": true,
         "dots": true
