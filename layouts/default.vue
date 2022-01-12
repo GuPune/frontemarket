35b092d2c4928556d5ea48255326070d8de589fc
Rkknoob <iboonkhet@gmail.com>
2022-01-12 22:51:52 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -54,11 +54,6 @@ import { FETCH_ID_URL } from "@/store/actions.type.js";
 import Vue from 'vue'
 import VueFbCustomerChat from 'vue-fb-customer-chat'
  
-Vue.use(VueFbCustomerChat, {
-  page_id: '4578428388940773', //  change 'null' to your Facebook Page ID,
-  theme_color: '#333333', // theme color in HEX
-  locale: 'en_US', // default 'en_US'
-})
 
 
     export default {
