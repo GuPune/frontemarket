83d96e5e9cfcde288a2fb147b7f090b65a311bef
Rkknoob <iboonkhet@gmail.com>
2022-01-12 22:56:27 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,7 +79,7 @@ export default {
    // "~/plugins/mixins/validation.js",
     // { src: '~/plugins/vue2-google-map.js'},
     { src: '~/plugins/vuelidate', ssr: true },
-    
+    { src: './plugins/vue-fb-customer-chat.js' },
     { src: './plugins/vue-slick-carousel.js' }
   ],
 
