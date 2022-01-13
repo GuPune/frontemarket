e640503442686f74644dd8ee3faa31a5154a04f7
boonkhet idklever <iboonkhet@gmail.com>
2022-01-13 11:53:39 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -64,7 +64,7 @@ import VueFbCustomerChat from 'vue-fb-customer-chat'
 
     data: () => ({
       
-    pageId: "1697883653756763",
+    pageId: "110934761475251",
     form:{
         url:null
     }
