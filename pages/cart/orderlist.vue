451e89cd756d82e46b4a617ea4daa8aad690846b
Rkknoob <iboonkhet@gmail.com>
2022-01-18 23:23:18 +0700

    Update orderlist.vue

--- pages/cart/orderlist.vue
+++ pages/cart/orderlist.vue
@@ -49,7 +49,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                                 <div class="row">
                                     <div class="col-xs-3 col-sm-12 col-md-3 col-lg-3">
                                         <div class="title-top h4">ราคา</div>
