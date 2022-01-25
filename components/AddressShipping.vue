e49704e9373649c4cffbe9ed20d8f53a6e4ad153
boonkhet idklever <iboonkhet@gmail.com>
2022-01-25 20:41:06 +0700

    Update AddressShipping.vue

--- components/AddressShipping.vue
+++ components/AddressShipping.vue
@@ -56,7 +56,7 @@
       </b-modal>
 
 
-    <b-modal id="modal-2"  title="เพิ่มที่อยู่" size="lg" no-close-on-backdrop>
+    <b-modal id="modal-2"  title="เพิ่มที่อยู่" size="lg" no-close-on-backdrop hide-footer>
         <div>
 
 
