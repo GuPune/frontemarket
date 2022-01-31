c66c4f5edbcb1ebf9a4aadb398e26360ed664ae7
boonkhet idklever <iboonkhet@gmail.com>
2022-01-31 23:40:24 +0700

    update
    update

--- pages/form/login.vue
+++ pages/form/login.vue
@@ -290,7 +290,8 @@ this.form.url = window.location.hostname;
 
 
        //  window.location.href = `https://cmsecom.idtest.work/api/auth/login/${service}`;
-                window.location.href = `backoffice.thaionzon.com/api/auth/login/${service}/con/${this.form.url}`;
+             //   window.location.href = `https://cmsecom.idtest.work/api/auth/login/${service}/con/${this.form.url}`;
+                   window.location.href = `https://backoffice.thaionzon.com/api/auth/login/${service}/con/${this.form.url}`;
         },
         async loginline(){
    this.$router.push('/1/form/register');   
