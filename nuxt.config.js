83885645c400d3a11c39f9dffcd6b49979dae44d
boonkhet idklever <iboonkhet@gmail.com>
2022-02-01 08:32:16 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/assets/ROGO_THAIONSON.png' }],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -18,9 +18,7 @@ export default {
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js' },
       { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }  
-    ],
+    
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
