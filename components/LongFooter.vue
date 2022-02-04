2f8917bec8ece8e4d269efce652041dc26bf62d2
Rkknoob <iboonkhet@gmail.com>
2022-02-04 13:15:59 +0700

    update
    update

--- components/LongFooter.vue
+++ components/LongFooter.vue
@@ -17,14 +17,16 @@
                                     </div>
                                     
                                     <div class="col-lg-3 col-xs-12 links" >
-                                        <h4 class="mt-lg-0 mt-sm-3" style="color:#e67825;">Links</h4>
+                                        <h4 class="mt-lg-0 mt-sm-3" style="color:#e67825;"></h4>
                                             <ul class="m-0 p-0">
+                                             <!-- 
                                             <li>- <a href="#">Lorem ipsum</a></li>
                                             <li>- <a href="#">Nam mauris velit</a></li>
                                             <li>- <a href="#">Etiam vitae mauris</a></li>
                                             <li>- <a href="#">Fusce scelerisque</a></li>
                                             <li>- <a href="#">Sed faucibus</a></li>
                                             <li>- <a href="#">Mauris efficitur nulla</a></li>
+                                            -->
                                             </ul>
                                     </div>
                                     <div class="col-lg-4 col-xs-12">
