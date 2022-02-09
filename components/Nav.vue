596316c07a81fd911f58a9cde9f8d7ed2af0b254
boonkhet idklever <iboonkhet@gmail.com>
2022-02-09 13:33:22 +0700

    update
    update

--- components/Nav.vue
+++ components/Nav.vue
@@ -204,7 +204,7 @@
 
     <b-navbar toggleable="sm" type="dark"   :style="{'background-color':objectslayout.color}" class="banav navbar-fixed-top" fixed="top">
     <b-navbar-brand  @click="redirectTo('index')">
-   <img src="../assets/ROGO_THAIONSON.png"  alt=""  class="icon-mobile">
+   <img src="../assets/neu-logo.png"  alt=""  class="icon-mobile">
     </b-navbar-brand>
               <b-navbar-brand href="#" class="nav-sell"  @click="redirectTo('form-shopregis')" >{{objectslayout.textsellermyshop}}</b-navbar-brand>
 
