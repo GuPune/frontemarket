ea1bb1b875a5bfc95710b1f8bd8df9b684ef7997
Rkknoob <iboonkhet@gmail.com>
2022-02-24 13:56:02 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Thaionzon | Thailand ซื่อขายสินค้าออนไลน์', href: '/favicon.ico' },
+      { hid: 'description', name: 'description', content: 'Thaionzon | Thailand ซื่อขายสินค้าออนไลน์', href: 'https://cmsecom.idtest.work/public/product/202111220826582VUbS2ECA0oGhGD62Jfh.jpg' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     script: [
