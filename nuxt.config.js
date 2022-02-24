72c08ae4bec6c4b0c4418e7f4e4c1f9ad789277e
Rkknoob <iboonkhet@gmail.com>
2022-02-24 12:28:25 +0700

    update
    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,18 +17,13 @@ export default {
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js' },
       { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js' },
     ],
-
     link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         type: 'text/css',
         href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
-      }
-    ],
-
-    link: [
-
-      { rel: 'icon', type: 'image/x-icon', href: "https://s.yimg.com/rz/l/favicon.ico" },
+      },
       {
         rel: 'stylesheet',
         type: 'text/css',
