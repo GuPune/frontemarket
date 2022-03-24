e8cbfa0c0c39f1c7c2d5932ccf1a768ffb0b7e06
boonkhet idklever <iboonkhet@gmail.com>
2022-03-24 15:41:09 +0700

    update
    update

--- components/AddressShipping.vue
+++ components/AddressShipping.vue
@@ -2,7 +2,7 @@
 
         <div class="card shopping-cart">
 
-        <h2 class="shoping-cart-title bg-order"><span>ที่อยู่ในการจัดส่ง </span>
+        <h2 class="shoping-cart-title bg-order"><span>{{placeholder_shipping}} </span>
             <span id="showLinkChangeAddress">
             <a class="linkChangeAddress">  <b-button v-b-modal.modal-1 class="bt-ship"  @click="showModal()">{{placeholder_change}}</b-button></a>
                    <a class="linkChangeAddress">  <b-button v-b-modal.modal-2 class="bt-ship">{{placeholder_add}}</b-button></a>
@@ -27,7 +27,7 @@
     </div>
     </div>
 
-      <b-modal id="modal-1"  title="เลือกที่อยู่ในการจัดส่ง" size="lg">
+      <b-modal id="modal-1"  :title="[[ placeholder_choose_add ]]" size="lg">
         <div>
 
 
@@ -35,7 +35,7 @@
           <table class="table table-striped">
     <thead>
       <tr>
-        <th>ที่อยู่</th>
+        <th>{{placeholder_address}}</th>
         <th></th>
 
       </tr>
@@ -335,6 +335,8 @@ export default {
          this.placeholder_shipping = 'Choose a delivery address';
           this.placeholder_change = 'Change';
           this.placeholder_add = 'Add';
+           this.placeholder_choose_add = 'Choose a delivery address';
+           this.placeholder_address = 'Address';
 
 
         }
@@ -342,6 +344,8 @@ export default {
           this.placeholder_shipping = '选择送货地址';
                 this.placeholder_change = '更换地址';
           this.placeholder_add = '添加地址';
+          this.placeholder_choose_add = '选择送货地址';
+          this.placeholder_address = 'Address';
 
 
         }
@@ -349,8 +353,8 @@ export default {
           this.placeholder_shipping = 'ที่อยู่ในการจัดส่ง';
           this.placeholder_change = 'เปลี่ยนที่อยู่';
           this.placeholder_add = 'เพิ่มที่อยู่';
-
-
+          this.placeholder_choose_add = 'เลือกที่อยู่ในการจัดส่ง';
+          this.placeholder_address = 'ที่อยู่';
         }
 
 
