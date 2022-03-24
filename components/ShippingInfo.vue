105945327c517aafee606ae2c0ef295c4284c491
boonkhet idklever <iboonkhet@gmail.com>
2022-03-24 14:58:03 +0700

    update
    update

--- components/ShippingInfo.vue
+++ components/ShippingInfo.vue
@@ -1,9 +1,9 @@
 <template>
         <div class="card shopping-cart">
-                  <h2 class="shoping-cart-title bg-order"><span>วิธีการจัดส่ง</span></h2>
+                  <h2 class="shoping-cart-title bg-order"><span>{{placeholder_deli}}</span></h2>
                         <div class="card-body"  v-for="(data, index) in shipping" :key="data.id">
                 <div class="form-group mb-0" id="boxCardShipping">
-                <div class="row boxPaymentList pb-3 mb-3"> 
+                <div class="row boxPaymentList pb-3 mb-3">
                 <div class="col-12 col-md-4 col-xs-12" >
                     <label>
                          <input type="radio"   name="profileImg"   @change="changeAdd($event)" :value="data.id" v-model="selectedDel">&nbsp; {{data.name}}
@@ -14,20 +14,20 @@
                 <div class="col-12 col-md-5 col-xs-12">
                     <div class="text-center"><img class="shippingImage fill responsive" :src="Checkimage(data.avatar)"  width="300" height="100" />
                     </div>
-                </div> 
+                </div>
                 <div class="col-12 col-md-3 col-xs-12">
                     <span>ฟรี</span>
                 </div>
-          
+
             </div>
-            
+
             </div>
               <div>
 
    <div>
   </div>
   </div>
-                        </div>    
+                        </div>
             </div>
 </template>
 
@@ -40,7 +40,7 @@ import { mapGetters } from "vuex";
 import { CHOOSE_DELIVERY,CHECK_DELIVERY,DELIVERY_DATA } from "../store/actions.type.js";
   export default {
     data() {
-      
+
       return {
         status: 'not_accepted',
         selectedAdd: null,
@@ -60,14 +60,37 @@ import { CHOOSE_DELIVERY,CHECK_DELIVERY,DELIVERY_DATA } from "../store/actions.t
                 return this.$store.state.user.url_id;
           },
 
-        
+
+        },
+
+                async created(){
+
+
+        this.language = localStorage.getItem("language");
+
+        if(this.language == 'en'){
+         this.placeholder_deli = 'Shipping method';
+
+
+        }
+        if(this.language == 'ch'){
+          this.placeholder_deli = '邮寄方式';
+
+
+        }
+        if(this.language == 'th' || this.language == null){
+          this.placeholder_deli = 'วิธีการจัดส่ง';
+
+
+        }
+
         },
 
      async mounted() {
        this.form.url = window.location.origin
         let delivery_data = await this.$store.dispatch(DELIVERY_DATA,this.form);
         this.shipping = delivery_data;
-       
+
  let check = await localStorage.getItem('delivery');
  if(check){
    this.selectedDel = check;
@@ -81,11 +104,11 @@ import { CHOOSE_DELIVERY,CHECK_DELIVERY,DELIVERY_DATA } from "../store/actions.t
                 return public_images;
         },
 
-  
+
         changeAdd(event){
            this.selectedDel = event.target.value
            let delivery =  this.$store.dispatch(CHOOSE_DELIVERY,this.selectedDel);
-    
+
         },
       }
   }
