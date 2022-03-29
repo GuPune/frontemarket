9304f763db1bca6a8c7edec730ea65085a2ea34f
Rkknoob <iboonkhet@gmail.com>
2022-03-30 00:08:22 +0700

    update
    update

--- components/Nav.vue
+++ components/Nav.vue
@@ -155,7 +155,7 @@
           </b-navbar-brand>
       </b-col>
 
-          <b-col cols="2" md="2"  sm="2" lg="2" class="px-2 nav-mobile-fav">
+          <b-col cols="2" md="2"  sm="2" lg="2" class="px-2">
           <b-navbar-brand href="#">
             <i class="fa fa-heart-o regishover" aria-hidden="true"></i><span class="descquickmenu regishover" >รายการโปรด</span>
 
@@ -176,6 +176,91 @@
           <!-- Using 'button-content' slot -->
           <template #button-content>
             <em>  <i class="fa fa-user-circle-o" aria-hidden="true"></i><span class="descquickmenu" style="color:#ffffff">คุณ{{ objects.name}}</span></em>
+          </template>
+          <b-dropdown-item href="#" @click="redirectTo('profile-userprofile')">
+            <div>
+
+              <div v-if ="this.language == 'en'" > {{this.placeholder_profile}} </div>
+             <div v-if ="this.language == 'ch'" > {{this.placeholder_profile}}</div>
+              <div v-if ="this.language == 'th' || this.language == null" > {{this.placeholder_profile}} </div>
+            </div>
+          </b-dropdown-item>
+          <b-dropdown-item href="#"  @click.prevent="logout">
+               <div v-if ="this.language == 'en'" > {{this.placeholder_logout}} </div>
+             <div v-if ="this.language == 'ch'" > {{this.placeholder_logout}}</div>
+              <div v-if ="this.language == 'th' || this.language == null" > {{this.placeholder_logout}} </div>
+
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+            </b-col>
+       </b-row>
+
+
+        <b-row>
+     <b-col cols="8" md="8"  sm="8" lg="8" class="nav-form-search nav-search search-mobile">
+              <b-form-input placeholder="Find a good product ?" v-model="form.search" v-if ="this.language == 'en'"></b-form-input>
+              <b-form-input placeholder="寻找好的产品 ?" v-model="form.search" v-if ="this.language == 'ch'"></b-form-input>
+              <b-form-input placeholder="ค้นหาสินค้าอะไรดี ?" v-model="form.search" v-if ="this.language == 'th' || this.language == null"></b-form-input>
+      </b-col>
+     <b-col cols="3" md="3"  sm="3" lg="3" class="nav-form-search">
+  <b-form-select v-model="selected" :options="options"></b-form-select>
+      </b-col>
+      <b-col cols="1" md="1"  sm="1" lg="1" class="nav-form-search">
+
+
+  <b-button variant="outline-primary ic-nav-bg-white" @click="Search()">   <i class="fas fa  fas fa-search ic-nav-black" aria-hidden="true"></i></b-button>
+
+      </b-col>
+  </b-row>
+    </b-navbar-brand>
+
+
+  </b-navbar>
+
+  </b-row>
+</b-container>
+
+
+
+<b-container class="bv-example-row nav-first-s nav-mobile-smal-last" v-if="objectslayout">
+  <b-row>
+
+    <b-navbar toggleable="sm" type="dark"   :style="{'background-color':objectslayout.color}" class="banav navbar-fixed-top" fixed="top">
+    <b-navbar-brand class="nav_mobie_fisrt">
+       <b-row>
+           <b-col cols="2" md="2"  sm="2" xl="3" >
+           <img src="../assets/log.png"  alt=""  class="icon-mobile nav-icon-mobile">
+            </b-col>
+    
+
+        <b-col cols="3" md="2"  sm="2" class="px-2">
+          <b-navbar-brand href="#"   @click="redirectTo('form-shopregis')"><span class="descquickmenumobile regishover">{{this.placeholder_mysell}} | </span>
+
+          </b-navbar-brand>
+      </b-col>
+
+
+       <b-col cols="3" md="2"  sm="2" class="px-2">
+          <b-navbar-brand href="#"   @click="redirectTo('cart-orderlist')"><i class="fas fa fa-cart-plus fa-w-18 icon-size-nav regishover" aria-hidden="true"></i><span class="numitem "> {{ cartTotal }}  </span><span class="descquickmenumobile regishover"> ตะกร้าสินค้า |</span>
+
+          </b-navbar-brand>
+      </b-col>
+
+  
+
+      <b-col cols="4" md="4"  sm="4" lg="3" class="nav-form-search px-2 login-size-ipad">
+          <b-navbar-brand href="#"   v-if="!isLogins">
+                 <i class="fa fa-user-circle-o" aria-hidden="true"></i><span class="descquickmenumobile">
+                   <span @click="redirectTo('form-login')" class="regishover"> {{this.placeholder_login}}</span></span>
+            </b-navbar-brand>
+
+
+                      <b-navbar-nav class="ml-auto cart-desktop">
+          <b-nav-item-dropdown right  v-if="isLogins">
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>  <i class="fa fa-user-circle-o" aria-hidden="true"></i><span class="descquickmenumobile" style="color:#ffffff">คุณ{{ objects.name}}</span></em>
           </template>
           <b-dropdown-item href="#" @click="redirectTo('profile-userprofile')">
             <div>
