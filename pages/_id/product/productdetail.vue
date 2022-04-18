15e60c6d979ddff9776b4f23d93cf6f454012e08
boonkhet idklever <iboonkhet@gmail.com>
2022-04-18 17:08:04 +0700

    Update productdetail.vue

--- pages/_id/product/productdetail.vue
+++ pages/_id/product/productdetail.vue
@@ -612,7 +612,7 @@ let images_product = await this.$store.dispatch(FETCH_IMAGE_PRODUCT,this.form);
             },
 
          async addfav(){
-           console.log('this.authenticated',this.$auth.isLoggedIn);
+           console.log('this.authenticated',this.$auth.user);
        if(!this.authenticated){
        }else{
          this.active_el = 1;
