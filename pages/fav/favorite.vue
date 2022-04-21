dfd37ecee03295a62369e428d78c785015f54d95
boonkhet idklever <iboonkhet@gmail.com>
2022-04-21 10:53:28 +0700

    Update favorite.vue

--- pages/fav/favorite.vue
+++ pages/fav/favorite.vue
@@ -1,7 +1,7 @@
 <template>
 
 
-   <div class="container col-9" style="padding-top: 90px;">
+   <div class="container col-12 col-xl-9 col-sm-12" style="padding-top: 90px;">
   <Nav />
 <!-- --------------------------------store 1--------------------------------- -->
 <div class="row" style="padding-top: 40px;">
