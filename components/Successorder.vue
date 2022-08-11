b5b22838069899e355efcd44cf13433334c544c8
iboonkhet@gmail.com <iboonkhet@gmail.com>
2022-08-11 15:53:52 +0700

    Update Successorder.vue

--- components/Successorder.vue
+++ components/Successorder.vue
@@ -686,21 +686,7 @@ import axios from 'axios';
 
         downloadpdf () {
 
-axios({
-                    url: 'http://cmsecom.idtest.work/api/get-file/'+this.objectss.cartnumber,
-                    method: 'GET',
-                    responseType: 'blob',
-                }).then((response) => {
-                     var fileURL = window.URL.createObjectURL(new Blob([response.data]));
-                     var fileLink = document.createElement('a');
-
-                     fileLink.href = fileURL;
-                     fileLink.setAttribute('download', 'file.pdf');
-                     document.body.appendChild(fileLink);
-
-                     fileLink.click();
-                });
-
+alert('downloadpdf');
 			},
 
           scrollToTop() {
