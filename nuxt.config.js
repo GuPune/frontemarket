242da74d7363d78b16422311fd58f24ad406eb7a
iboonkhet@gmail.com <123219570+Rkknoob1@users.noreply.github.com>
2023-03-27 09:10:38 +0700

    update
    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -147,17 +147,17 @@ export default {
 
 axios: {
   // See https://github.com/nuxt-community/axios-module#options
-  baseURL: "http://back.nanoik.com/api",
-  ImageURL: "http://back.nanoik.com/public/product/",
-  ImageSoc: "http://back.nanoik.com/public/images/",
+  baseURL: "https://back.ftikkmart.com/api",
+  ImageURL: "https://back.ftikkmart.com/public/product/",
+  ImageSoc: "https://back.ftikkmart.com/public/images/",
 
  },
 
 env: {
- baseURL: "http://back.nanoik.com/api",
-  ImageURL: "http://back.nanoik.com/public/product/",
-  ImageSoc: "http://back.nanoik.com/public/images/",
-  backend:"http://back.nanoik.com",
+ baseURL: "https://back.ftikkmart.com/api",
+  ImageURL: "https://back.ftikkmart.com/public/product/",
+  ImageSoc: "https://back.ftikkmart.com/public/images/",
+  backend:"https://back.ftikkmart.com",
 },
 
 
