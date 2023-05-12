7c4d1ee35ec5f4f2db9551a6cfede031660793ef
iboonkhet@gmail.com <123219570+Rkknoob1@users.noreply.github.com>
2023-05-12 16:38:49 +0700

    Update environment.js

--- environment/environment.js
+++ environment/environment.js
@@ -1,5 +1,5 @@
 //export const API_URL = process.env.MIX_APP_URL;
 //export const API_URL = "http://127.0.0.1:8000/api";
-export const API_URL = "https://back.nanoik.com/api";
+//export const API_URL = "https://back.nanoik.com/api";
 
-//export const API_URL = "https://backoffice.thaionzon.com/api";
+export const API_URL = "https://backoffice.thaionzon.com/api";
