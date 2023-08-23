2546ffed7f8a169065e1ed2378a7cfbae41c66d9
Rkknoob <iboonkhet@gmail.com>
2023-08-24 01:24:11 +0700

    Update gtag.js

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -4,17 +4,17 @@ import axios from 'axios'
 
 let a = window.location.origin;
 
-axios.post('https://backend.pra84000.com/api/gtag', {
-    data: a
-  }).then(res => {
+// axios.post('https://backend.pra84000.com/api/gtag', {
+//     data: a
+//   }).then(res => {
 
- Vue.use(VueGtag, {
-    config: { id: res.data }
-  })
-  }).catch(function(){
-     
-  
-  
-});
+//  Vue.use(VueGtag, {
+//     config: { id: res.data }
+//   })
+//   }).catch(function(){
+
+
+
+// });
 
 
